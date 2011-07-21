f687d04e4f67b91dc9cc91c8ba91c64e7bbaec6b
Joe Osborn <joe.osborn@mac.com>
2011-07-21 09:09:27 -0700

    slight copyright tweak

--- UIImage_ImageHelper.m
+++ UIImage_ImageHelper.m
@@ -2,6 +2,7 @@
  * The MIT License
  *
  * Copyright (c) 2011 Paul Solt, PaulSolt@gmail.com
+ * Modifications Copyright (c) 2011 Joe Osborn, josborn@universalhappymaker.com
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
